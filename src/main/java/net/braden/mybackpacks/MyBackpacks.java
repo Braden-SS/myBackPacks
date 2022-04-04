8db26ecca134f8ae899d9a3ac6c217a85b36c729
Braden-SS <102992182+Braden-SS@users.noreply.github.com>
2022-04-04 11:40:06 -0700

    Initial commit! I'm Braden!

---
+++ src/main/java/net/braden/mybackpacks/MyBackpacks.java
@@ -0,0 +1,16 @@
+package net.braden.mybackpacks;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyBackpacks implements ModInitializer {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
